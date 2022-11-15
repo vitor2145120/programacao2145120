4732c0739f5497eead382722a8f29a3df926c2f0
vitor2145120 <vitor375@hotmail.com>
2022-11-15 09:42:04 +0000

    testedebug

---
+++ testedebug/src/testedebug.c
@@ -0,0 +1,38 @@
+/*
+ ============================================================================
+ Name        : testedebug.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void drawLine(int width){
+		for (int i = 0; i < width; i++){
+			printf("%c" , " * ");
+
+		}
+	}
+int main(void) {
+
+
+	int a = 10;
+	int b = 2;
+
+	int c = a * b;
+
+
+
+
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
