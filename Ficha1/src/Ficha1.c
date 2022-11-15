9b10f5887980536b9eb072e2af734eab8f87473e
vitor2145120 <vitor375@hotmail.com>
2022-11-15 09:41:16 +0000

    ficha1

--- Ficha1/src/Ficha1.c
+++ Ficha1/src/Ficha1.c
@@ -12,6 +12,25 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	int i;
+
+//scanf("%d%f",&i,&f);
+//		printf("val of i is %d\n val of f is %f\n",i,f);
+
+	int x1 = 1;
+	int y1 = 2;
+
+	int x2 = 3;
+	int y2 = 3;
+
+	float f = (x1 * y2);
+
+
+
+
+
+
+ 	return 0;
+
 }
+
